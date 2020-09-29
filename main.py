3963ba3b9798d0cd2e6452c355676700b37da738
izzyevermore <isaiahlekay2001@gmail.com>
2020-09-29 11:25:19 +0200

    python recursion exercise

---
+++ main.py
@@ -0,0 +1,14 @@
+# Check if a number is a palindrome
+def is_palindrome(a_string):
+    if len(a_string) <= 1:
+        return True
+    if a_string[0] == a_string[len(a_string) - 1]:
+        return is_palindrome(a_string[1:-1])
+    else:
+        return False
+
+a_string = input("Please input a number: ")
+print(is_palindrome(a_string))
+
+
+
